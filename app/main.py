0094b58aeb483e1040a6c9321e59c2c84c16ead2
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2021-04-23 08:57:35 +0200

    Dockerized application

---
+++ app/main.py
@@ -0,0 +1,33 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routers import discs
+
+
+def get_app():
+  app = FastAPI(title="Discinstock_api")
+
+  # CORS
+  origins = {
+    "http://localhost:8081"
+  }
+
+  app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+  )
+
+  # Routes
+  app.include_router(discs.router)
+
+  @app.get("/", tags=["Root"])
+  async def read_root():
+    return {"API to find discgolf discs in stock in diffrent websites where you can buy discs"}
+
+  return app
+
+
