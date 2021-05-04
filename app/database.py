a5867fb394aa78697ee8afc208764c76a5f67d3a
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2021-05-04 10:07:17 +0200

    Added env vars for connecting to db

--- app/database.py
+++ app/database.py
@@ -1,9 +1,29 @@
 import motor.motor_asyncio
+import os
 
 from bson.objectid import ObjectId
 
+def get_db():
+  MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+  MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+  MONGO_DB = os.getenv("MONGO_DB", "database")
+  MONGO_NON_ROOT_USERNAME = os.getenv("MONGO_NON_ROOT_USERNAME", "user")
+  MONGO_NON_ROOT_PASSWORD_FILE = os.getenv("MONGO_NON_ROOT_PASSWORD_FILE")
 
-MONGO_URI = "mongodb://192.168.10.5:27017"
+  if MONGO_NON_ROOT_PASSWORD_FILE:
+    with open(MONGO_NON_ROOT_PASSWORD_FILE, "r") as file:
+      MONGO_NON_ROOT_PASSWORD = file.read()
+  else:
+    MONGO_NON_ROOT_PASSWORD = "Passw0rd"
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = client.discgolfspider
+  uri = f"mongodb://{MONGO_NON_ROOT_USERNAME}:{MONGO_NON_ROOT_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/?authSource={MONGO_DB}"
+
+  print(f"URI={uri}")
+
+  client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+  db = client[MONGO_DB]
+
+  return db
+
+
+db = get_db()
